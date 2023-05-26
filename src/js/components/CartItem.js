8a6d4c28edd8b53fb8f96f3b2b3c27a5167aae09
Mari4ka89 <mari4ka89@gmail.com>
2023-05-26 14:14:42 +0300

    Updated styles for carts items

--- src/js/components/CartItem.js
+++ src/js/components/CartItem.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { UPDATE_PRODUCT_IN_CART } from "../constants/actionTypes";
+import "../../css/CartItem.css";
 
 export default function CartItem({ productId, quantity, image, title, price }) {
   const dispatch = useDispatch();
@@ -8,29 +9,27 @@ export default function CartItem({ productId, quantity, image, title, price }) {
     dispatch({
       type: UPDATE_PRODUCT_IN_CART,
       productId,
-      quantity: parseInt(e.target.value),
+      quantity: parseInt(e.target.value || 0),
     });
   }
 
   return (
-    <div className="card mb-3">
+    <div className="CartItem card mb-3">
       <div className="row g-0">
         <div className="col-md-4">
           <img src={image} className="img-fluid rounded-start" alt={title} />
         </div>
         <div className="col-md-8">
-          <div className="card-body">
+          <div className="card-body text-center">
             <h5 className="card-title">{title}</h5>
             <h6>Price: {price}₴</h6>
-            <p className="card-text">
-              <input
-                type="number"
-                className="form-control"
-                onChange={handleAmountChange}
-                defaultValue={quantity}
-                min="0"
-              />
-            </p>
+            <input
+              type="number"
+              className="form-control w-50"
+              onChange={handleAmountChange}
+              defaultValue={quantity}
+              min="0"
+            />
           </div>
         </div>
       </div>
